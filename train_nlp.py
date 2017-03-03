f1527465640c554ea7086a41765026c783136029
Jared Dunnmon <jdunnmon@scail.stanford.edu>
2017-03-02 16:49:39 -0800

    added job scripts

--- train_nlp.py
+++ train_nlp.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
 
+from __future__ import absolute_import, division, print_function
+from builtins import *
+
 import tensorflow as tf
 import numpy as np
 import os
@@ -14,19 +17,19 @@ from tensorflow.contrib import learn
 
 # Data loading params
 tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("sentences_data_file", "./nlp_features/gop_sentences.npy", "Data source for the positive data.")
+tf.flags.DEFINE_string("sentences_data_file", "./nlp_features/gop_sentences_list_of_strings.npy", "Data source for the positive data.")
 tf.flags.DEFINE_string("labels_data_file", "./nlp_features/gop_onehotlabels.npy", "Data source for labels.")
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
+tf.flags.DEFINE_string("filter_sizes", "1,2,3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 10, "Number of training epochs (default: 200)")
+tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
 tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
@@ -49,15 +52,16 @@ print("")
 print("Loading data...")
 #x_text, y = data_helpers.load_data_and_labels(FLAGS.positive_data_file, FLAGS.negative_data_file)
 
-x_text = np.load(FLAGS.sentences_data_file,encoding='bytes')
-y = np.load(FLAGS.labels_data_file,encoding='bytes')
+x_ld = np.load(FLAGS.sentences_data_file)
+y = np.load(FLAGS.labels_data_file)
 
-import pdb; pdb.set_trace()
-
+x_text = [str(x) for x in x_ld] 
 # Build vocabulary
 max_document_length = max([len(x.split(" ")) for x in x_text])
 vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
 x = np.array(list(vocab_processor.fit_transform(x_text)))
+
+#import pdb; pdb.set_trace()
 # Randomly shuffle data
 np.random.seed(10)
 shuffle_indices = np.random.permutation(np.arange(len(y)))
